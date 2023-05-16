18a911e8e0479961e974dc619e0678bcfa392d9a
Gurman Banwait <Banwait_Gurman@outlook.com>
2023-05-15 22:23:50 -0400

    Initial Setup

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+//npm run devStart
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+module.exports = router
+
+
